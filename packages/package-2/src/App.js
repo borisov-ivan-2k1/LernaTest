8183d23c21f38f9ebcc4c7316db623c164ea8a36
Borisov Ivan <borisov.ivan.2001.rkn@gmail.com>
2022-01-20 00:06:26 +0300

    p2-change

--- packages/package-2/src/App.js
+++ packages/package-2/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          test 222
+          test
         </p>
         <a
           className="App-link"
